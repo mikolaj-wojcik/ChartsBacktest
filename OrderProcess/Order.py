515eae5aaae5b37f522c058e73cc50aa3775c1db
mikolaj-wojcik <mikolwoj@gmail.com>
2024-02-13 07:29:19 +0100

    Edited transaction module

---
+++ OrderProcess/Order.py
@@ -0,0 +1,111 @@
+from enum import Enum
+
+
+
+class Order():
+    def __init__(self, size, side, oType,protectionOrder :dict,  price=0.0, candle = None,  ) -> None:
+        self.price = price
+        self.size = size
+        self.candle  = candle
+        self.closeCandle = None
+        self.direction = side
+        self.closePrice = None
+        self.orderType = oType
+        self.stopLoss =0.0
+        self.takeProfit = 0.0
+       # if(protectionOrder):
+        if protectionOrder['stop loss'] != 0.0:
+            if((self.direction == Direction.BUY and self.price > protectionOrder['stop loss']) or (self.direction == Direction.SELL and self.price <protectionOrder['stop loss'])):
+                self.stopLoss = protectionOrder['stop loss']
+        if protectionOrder['take profit'] != 0.0:
+            if((self.direction == Direction.BUY and self.price < protectionOrder['take profit']) or (self.direction == Direction.SELL and self.price > protectionOrder['take profit'])):
+                self.takeProfit = protectionOrder['take profit']
+
+    def setStopLoss(self, openPrice, stopLoss):
+        if(self.direction == Direction.BUY):
+            if(stopLoss < openPrice):
+                self.stopLoss = stopLoss
+            pass
+        elif(self.direction == Direction.SELL):
+            if(stopLoss > openPrice):
+                self.stopLoss = stopLoss
+            pass
+        pass
+    
+    def setTakeProfit(self, openPrice ,takeProfit):
+        if(self.direction == Direction.BUY):
+            if(takeProfit > openPrice):
+                self.takeProfit = takeProfit
+            pass
+        elif(self.direction == Direction.SELL):
+            if(takeProfit < openPrice):
+                self.takeProfit = takeProfit
+            pass
+        pass
+
+    def setClosePriceAndCandle(self, price, candle):
+        self.closePrice = price
+        self.closeCandle = candle
+
+    def setSize(self, size):
+        self.size = size
+    def checkTpSlHit(self, priceBar):
+        returnPrice = None
+        openP = priceBar['open']
+        high = priceBar['high']
+        low = priceBar['low']
+
+    #Limit orders (TakeProfits) 
+        if self.direction is Direction.BUY:
+            if high < self.takeProfit:
+                returnPrice = openP
+            elif self.takeProfit >= low:
+                if openP < self.takeProfit:  
+                    returnPrice = openP
+                else:
+                    returnPrice = self.takeProfit
+        elif self.direction is Direction.Sell:
+            if low > self.takeProfit:
+                returnPrice = openP
+            elif self.takeProfit <= high:
+                if openP > self.takeProfit:  
+                    returnPrice = openP
+                else:
+                    returnPrice = self.takeProfit
+
+    #Stop orders (StopLosses)
+         
+        if self.direction is Direction.BUY:
+            if low > self.stopLoss:
+                returnPrice = openP
+            elif self.stopLoss <= high:
+                if openP > self.stopLoss: 
+                    returnPrice = openP
+                else:
+                    returnPrice = self.stopLoss
+       
+        elif self.direction is Direction.SELL:
+            if high < self.stopLoss:
+                returnPrice = openP
+            elif self.stopLoss >= low:
+                if openP < self.stopLoss: 
+                    returnPrice = openP
+                else:
+                    returnPrice = self.stopLoss
+
+        return returnPrice
+
+
+
+class Direction(Enum):
+    BUY = 'buy'
+    SELL = 'sell'
+
+class Status(Enum):
+    PENDING = 0
+    FILLED = 1
+
+class OrderType(Enum):
+    MARKET = 'market'
+    LIMIT = 'limit'
+    STOP =  'stop'
---
+++ OrderProcess/Transaction.py
@@ -0,0 +1,343 @@
+import math
+import Order
+import copy
+
+class Transaction():
+    def __init__(self, balance, positions, leverage):
+        self.history =[] #closed orders list
+        self.balance = balance
+        self.positions = positions #open orders list
+        self.pending = []
+        self.leverage = leverage
+        self.equity = balance
+        self.floatingPL = 0.0
+        self.startBalance = balance
+        pass
+
+    def reset(self):
+        self.balance = self.startBalance
+        self.floatingPL =0.0
+        self.positions= []
+        self.history=[]
+     
+    def close(self, positions, balance, size, closeBuy,candle,  price, leverage = 1):
+        if(closeBuy): #close buy
+            while(size < 0):
+                if(len(positions) > 0):
+                    posToClose = positions.pop()
+                    if posToClose[0] > -size:
+                        positions.append((posToClose[0]+ size, posToClose[1]))
+                        balance -= size*price
+                        self.history.append((candle, size, price, 0.0))
+                        size = 0
+                    elif  posToClose[0] <= -size:
+                        balance += posToClose[0]*price
+                        size+=posToClose[0]
+                        self.history.append((candle, posToClose[0], price, 0.0, 0.0))
+                else:
+                    break
+            
+        else: #close sell
+            while(size > 0):
+                if(len(positions) > 0):
+                    posToClose = positions.pop()
+                    if posToClose[0] < -size:
+                        positions.append((posToClose[0]- size, posToClose[1]))
+                        self.history.append((candle, size, price, ))
+                        balance += size*price
+                        size = 0
+                    elif  posToClose[0] >= -size:
+                        balance -= posToClose[0]*price
+                        size+=posToClose[0]
+                        self.history.append((candle, posToClose[0], price))
+                else:
+                    break 
+
+        return positions, balance, size
+     
+
+    def selectLowPrice(self,  orderPrice, lastPrice, price):
+
+        if(lastPrice['low'] > orderPrice):
+            return price
+        else:
+            return orderPrice
+
+        pass
+
+    def selectHighPrice(self,  orderPrice, lastPrice, price):
+        if(lastPrice['high'] < orderPrice):
+            return price
+        else:
+            return orderPrice
+    
+    def closeSpecPosition(self,candle, positionIndex, price):
+            posToClose = self.positions.pop(positionIndex)
+            self.balance+= (price - posToClose[1])*posToClose[0] + abs(posToClose[0]*posToClose[1]/self.leverage)
+            self.history.append((candle, -posToClose[0], price))
+            pass
+    
+    def checkForSlTp(self, candle, price, lastPrice):
+        for ind, pos in enumerate(self.positions):
+
+            if (pos[2] != 0.0):
+                if(pos[0] > 0):
+                    if(max(lastPrice['high'], price) >= pos[2]):
+                        print('Take profit')
+                        orderPrice = self.selectHighPrice(lastPrice=lastPrice, price=price, orderPrice= pos[2])
+                        self.closeSpecPosition(candle, ind, orderPrice)
+                        
+                else:
+                    if(min(lastPrice['low'], price) <= pos[2]):  
+                        orderPrice = self.selectLowPrice(lastPrice=lastPrice, price=price, orderPrice= pos[2])
+                        self.closeSpecPosition(candle, ind, orderPrice)
+                        
+                
+            if(pos[3]!= 0.0):
+                if(pos[0] > 0):
+                    if(max(lastPrice['low'], price) <= pos[3]):
+                        print('Stop loss')
+                        orderPrice = self.selectLowPrice(lastPrice=lastPrice, price=price, orderPrice= pos[3])
+                        self.closeSpecPosition(candle, ind, orderPrice)
+                        
+                        
+                else:
+                    if(min(lastPrice['high'], price) >= pos[3]):  
+                        orderPrice = self.selectHighPrice(lastPrice=lastPrice, price=price, orderPrice= pos[3])
+                        self.closeSpecPosition(candle, ind, orderPrice)
+                        
+                pass
+            pass
+        return self.balance, self.positions
+        
+    
+          
+class NoLewerage(Transaction):
+
+    def __init__(self,balance, positions = [], ):
+        super().__init__(balance, positions, 1)
+        
+        self.marginPrice = 0.0
+  
+        pass
+    
+        
+    def close(self, positions,candle, balance, size, price):
+            self.positions, self.balance, size = super().close(balance = self.balance, price = price, size = -size, clooseBuy = True, positions = self.positions)
+            
+
+
+
+    def buy(self, price,candle, transactionHistory,  size = 1.0,takeProfit = 0.0, stopLoss = 0.0):
+        if(size*price < self.balance):
+                self.positions.append((size,price))
+                self.balance -= price*size
+                dict = {'take profit' : takeProfit, 'stop loss' : stopLoss}
+                
+                pos = Order.Order(size, Order.Direction.BUY, Order.OrderType.MARKET, dict, price= price, candle = candle, )
+                self.positions.append(pos)
+        else:
+                self.balance-= math.floor(self.balance/size) * price
+
+                pos = Order.Order(math.floor(self.balance/size), Order.Direction.BUY, Order.OrderType.MARKET, dict, price= price, candle = candle, )
+                self.positions.append(pos)
+        return self.balance, self.positions
+    
+
+    def sell(self, price,candle, transactionHistory, size = 1.0,takeProfit = 0.0, stopLoss = 0.0):
+             while size >0.0:
+                 if(len(self.positions) > 0):
+                     posToClose = self.positions.pop()
+                     if posToClose.size > size:
+                         closedPos = copy.deepcopy(posToClose)
+                         posToClose.size -= size
+                         closedPos.setSize(size)
+                         closedPos.setClosePriceAndCandle(price, candle)
+                         self.positions.append(posToClose)
+
+                         self.history.append(closedPos)
+                         self.balance += size*price
+                         size = -1
+                     elif  posToClose.size <= size:
+                         self.balance += posToClose.size*price
+                         posToClose.setClosePriceAndCandle(price, candle)
+                         self.history.append(posToClose)
+                         size-=posToClose.size          
+                 else:
+                     break
+            #self.positions, self.balance, size = super().close(balance = self.balance, closeBuy = True, price = price, size = -size, positions = self.positions)
+             return self.balance, self.positions, 
+        
+
+    def calculateFloating(self, price):
+        floatingPL = self.balance
+        for pos in self.positions:
+            floatingPL += pos.size * price
+        self.floatingPL = floatingPL
+        return self.floatingPL
+        
+
+
+    def closeAllPositions(self, positions, balance, candle,lastPrice,toClose):
+        assetsVal = 0.0
+        if(toClose):
+            for pos in self.positions:
+                balance += pos.size * lastPrice
+                pos.setClosePriceAndCandle(lastPrice, candle)
+                self.history.append(pos)
+            self.positions = []
+        else:
+            for pos in self.positions:
+                assetsVal += pos.size * lastPrice
+        return balance, assetsVal  
+    
+    def checkForStopOut(self, canndle, lastPrice):
+         return self.balance, self.positions
+    
+class Lewerage(Transaction):
+    def __init__(self, balance, positions = [], leverage=10, stopOut=  0.5  ):
+        super().__init__(balance, positions, leverage)
+        
+        self.stopOutLevel = stopOut
+        pass
+
+
+    def buy(self, price, transactionHistory, candle,  size = 1000.0, takeProfit = 0.0, stopLoss = 0.0):
+        if(len(self.positions) != 0):
+            if(self.positions[0].direction is Order.Direction.SELL):
+                self.positions, self.balance, size = self.close(positions= self.positions, balance=self.balance, size=size,candle =candle, closeBuy = False, price = price, )
+                
+                pass
+        if(size > 0):
+            
+            if(size*price/self.leverage < self.balance):
+                ord = Order.Order(size, Order.Direction.BUY, Order.OrderType.MARKET, {'take profit': takeProfit, 'stop loss': stopLoss}, price, candle)
+                self.positions.append(ord)
+                #self.history.append((candle, size, price))
+                self.balance -= price*size/self.leverage
+        return self.balance, self.positions
+    
+    def sell(self, price, transactionHistory, candle, size = 1.0, takeProfit = 0.0, stopLoss =0.0):
+        #size *= 1
+        if(len(self.positions) != 0):
+            if(self.positions[0].direction is Order.Direction.BUY):
+                self.positions, self.balance, size = self.close(positions= self.positions, balance= self.balance, size= size, candle =candle,closeBuy = True, price = price, )
+                pass
+        if size >0:
+             if(-size*price/self.leverage < self.balance):
+                self.positions.append((size,price, takeProfit, stopLoss))
+                Order.Order(size, Order.Direction.SELL, Order.OrderType.MARKET, {'take profit': takeProfit, 'stop loss': stopLoss}, price, candle)
+                self.balance -= price*size/self.leverage
+                
+        return self.balance, self.positions
+        
+    def close(self, positions, balance, size, candle,  closeBuy, price):
+        if(closeBuy): #close buy
+            while(size > 0):
+                if(len(self.positions) > 0):
+                    posToClose = self.positions.pop()
+                    if posToClose.size > size:
+                        posToClose.size -= size
+                        self.positions.append(posToClose)
+                        self.balance += (size*(price - posToClose.price) + size*posToClose.price/self.leverage)
+                        copyPos = copy.deepcopy(posToClose)
+                        copyPos.size = size
+                        copyPos.closeCandle = candle
+                        copyPos.closePrice = price
+                        self.history.append(copyPos)
+                        size = 0
+                    elif  posToClose.size <= size:
+                        self.balance += posToClose.size*(price-posToClose.price) + posToClose[0]*posToClose[1]/self.leverage
+                        size-=posToClose[0]
+                        self.history.append((candle, -posToClose[0], price))
+                else:
+                    break
+            
+        else: #close sell
+            while(size > 0):
+                if(len(self.positions) > 0):
+                    posToClose = self.positions.pop()
+                    if posToClose[0] < -size:
+                        self.positions.append((posToClose[0]- size, posToClose[1]))
+                        self.balance += size*(posToClose[1] - price) + -size*posToClose[1]/self.leverage
+                        self.history.append((candle, size, price))
+                        size = 0
+                    elif  posToClose[0] >= -size:
+                        self.balance -= posToClose[0]*(posToClose[1] - price) + posToClose[0]*posToClose[1]/self.leverage
+                    
+                        size+=posToClose[0]
+                        self.history.append((candle, -posToClose[0], price))
+                else:
+                    break 
+
+        return self.positions, self.balance, size
+
+    def closeAllPositions(self, positions, balance,candle, lastPrice,toClose):
+        assetsVal = 0.0
+        if(toClose):
+            for pos in positions:
+                side =  pos[0] > 0.0
+                if side:
+                    self.positions, self.balance, size = self.close(positions= self.positions, balance= self.balance, size= -pos[0], candle =candle,closeBuy = side, price = lastPrice, )
+                else:
+                    self.positions, self.balance, size = self.close(positions= self.positions, balance= self.balance, size= -pos[0], candle =candle,closeBuy = side, price = lastPrice, )
+        else:
+            assetsVal = self.calculateFloating(lastPrice)
+        return balance, assetsVal
+
+    def calculateFloating(self, price):
+        self.floatingPL = 0.0
+        for transaction in self.positions:
+            self.floatingPL += transaction[0] * (price - transaction[1])
+        return self.floatingPL
+    
+
+
+    def checkForStopOut(self, candle, price, lastPrice):
+        while(not self.calculateMargin(price)):
+            print(self.positions)
+            if(self.positions[0][0] > 0):
+                liqPrice = self.selectLowPrice(self.marginPrice, lastPrice, price)
+                #liqPrice = self.selectLowPrice(False, liqPrice, price, lastPrice)
+                self.positions, self.balance, size = self.close(positions= self.positions, balance=self.balance, size=-self.positions[0][0], candle= candle,closeBuy = True, price = liqPrice, )
+            else:
+                liqPrice = self.selectHighPrice(self.marginPrice, lastPrice, price)
+                #liqPrice = self.selectHighPrice(False, liqPrice, price, lastPrice)
+                self.positions, self.balance, size = self.close(positions= self.positions, balance= self.balance, size= -self.positions[0][0],  candle= candle,closeBuy = False, price = liqPrice, )
+            pass
+
+        return self.balance, self.positions
+    
+   # def calculateMargin(self, price): #retur True if no positio reductio is required
+        margin = 0.0
+        ogMargin = 0.0
+        if(len(self.positions) != 0 ):
+            margin +=abs(sum(list(map(lambda x: x[0] * price /self.leverage, self.positions))))
+            ogMargin +=abs(sum(list(map(lambda x: x[0]*x[1] / self.leverage, self.positions))))
+            floating = self.calculateFloating(price)
+            self.marginPrice = self.stopOutLevel * margin
+            if(self.stopOutLevel * margin < ogMargin+ floating + self.balance):
+                return True
+            else:
+                return False
+        return True
+    
+    def calculateMargin(self, price): #retur True if no positio reductio is required
+        #margin = 0.0
+        #ogMargin = 0.0
+        if(len(self.positions) != 0 ):
+            a =abs(sum(list(map(lambda x: x[0] , self.positions))))
+            b =abs(sum(list(map(lambda x: x[0]*x[1], self.positions))))
+            floating = self.calculateFloating(price)
+            self.marginPrice = ((b/self.leverage) - b + self.balance)/((a*self.stopOutLevel/self.leverage)-a)
+            if(self.positions[0][0] > 0):
+                if(self.marginPrice < price):
+                    return True
+                else:
+                    return False
+            else:
+                if(self.marginPrice > price):
+                    return True
+                else:
+                    return False
+        return True
